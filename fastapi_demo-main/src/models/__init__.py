3db23cb7687ee5590db996cec1232824e03cd03b
Kisa6996 <nikita.rud.20015@mail.ru>
2023-01-18 11:23:00 +0300

    add file

---
+++ fastapi_demo-main/src/models/__init__.py
@@ -0,0 +1,11 @@
+from models.base import BaseModel
+from models.dish import Dish
+from models.menu import Menu
+from models.submenu import SubMenu
+
+__all__ = (
+    "BaseModel",
+    "Dish",
+    "Menu",
+    "SubMenu",
+)
